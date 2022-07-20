f99277e5c01d3ddf834f92cda20f681e03a005b4
hitchhicker <yubokai8@gmail.com>
2022-07-21 01:22:22 +0200

    Read description with utf-8 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 here = os.path.abspath(os.path.dirname(__file__))
